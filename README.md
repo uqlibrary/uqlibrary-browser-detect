7ab03dfff6854f667cfcb77177be5a291e9d4c66
jannakha <j_malikova@yahoo.com>
2016-03-22 16:21:19 +1000

    added browser update

--- README.md
+++ README.md
@@ -14,3 +14,25 @@ if (!data.supported) {
     console.log("Your browser is not supported");
 }
 ```
+
+## browser-update.js
+
+browser-update.js contains js to display a status bar in case browser is not supported
+
+To automatically inject this js:
+
+1. Insert //bower_components/uqlibrary-browser-supported/browser-update.js where this js needs to be inserted
+2. Add this gulp task to your build procedure
+
+```
+gulp.task('inject-browser-update', function() {
+
+var regEx = new RegExp("//bower_components/uqlibrary-browser-supported/browser-update.js", "g");
+var browserUpdate=fs.readFileSync("bower_components/uqlibrary-browser-supported/browser-update.js", "utf8");
+
+return gulp.src(<SOURCE>)
+    .pipe(replace({patterns: [{ match: regEx, replacement: browserUpdate}], usePrefix: false}))
+    .pipe(gulp.dest(<DESTINATION>))
+    .pipe($.size({title: 'inject-browser-update'}));
+});
+```
--- browser-update.js
+++ browser-update.js
@@ -1,12 +1,12 @@
   var $buoop = {
     vs: {i: 10, f: 30, o: 15, s: 7, c: 36},  // browser versions to notify
     reminder: 0,                   // after how many hours should the message reappear, 0 = show all the time
-    test: true,                    // true = always show the bar (for testing)
+    test: false,                    // true = always show the bar (for testing)
     text: "Your current browser is not on UQ Library\`s list of supported browsers. If you experience any problems with this site, please try using the site \
     with a different browser, or upgrade your current browser to the latest version. \
     If you are using assistive technology, please refer to our support for users with disabilities. \
     If you experience any problems with this site, there are Library staff who can provide assistance and supply information in a format that meets your needs. \
-    <a href='http://browser-update.org/update.html'>Download latest browsers here</a>",
+    <a href='http://browser-update.org/update.html'>Download latest browser here</a>",
     newwindow: true                 // open link in new window/tab
   };
 
