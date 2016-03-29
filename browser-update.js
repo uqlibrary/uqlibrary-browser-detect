ebdbd238ffca3a9546370ed5d33645c699d90427
jannakha <j_malikova@yahoo.com>
2016-03-29 16:01:51 +1000

    updated message

--- browser-update.js
+++ browser-update.js
@@ -1,12 +1,10 @@
   var $buoop = {
     vs: {i: 10, f: 30, o: 15, s: 7, c: 36},  // browser versions to notify
     reminder: 0,                   // after how many hours should the message reappear, 0 = show all the time
-    test: false,                    // true = always show the bar (for testing)
-    text: "Your current browser is not on UQ Library\`s list of supported browsers. If you experience any problems with this site, please try using the site \
-    with a different browser, or upgrade your current browser to the latest version. \
-    If you are using assistive technology, please refer to our support for users with disabilities. \
-    If you experience any problems with this site, there are Library staff who can provide assistance and supply information in a format that meets your needs. \
-    <a href='http://browser-update.org/update.html'>Download latest browser here</a>",
+    test: true,                    // true = always show the bar (for testing)
+    text: "Your current browser is not on UQ Library's <a href=\"https://www.library.uq.edu.au/site-information/web-browser-compatibility\">list of supported browsers</a>. " +
+    "If you experience any problems with this site, please try using the site with a different browser, or <a href=\"http://browser-update.org/update.html\">upgrade your current browser</a> " +
+    "to the latest version.",
     newwindow: true                 // open link in new window/tab
   };
 
